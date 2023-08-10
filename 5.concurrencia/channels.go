c950b17340afa1c227c65b514ccb79cae54210d3
ingporto <ing.portocarrero@gmail.com>
2023-08-10 15:09:56 -0500

    Unbuffered channels y buffered channels

---
+++ 5.concurrencia/channels.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	canalSinBuffer()
+}
+
+func canalSinBuffer() {
+	c := make(chan int)
+
+	c <- 1 // Se bloquea en esta línea
+	// fatal error: all goroutines are asleep - deadlock!
+
+	fmt.Println(<-c) // Espera retornar la información pero está bloqueado anteriomente
+}
+
+func canalConBuffer() {
+	c := make(chan int, 1)
+
+	c <- 1 // No se bloquea 
+
+	fmt.Println(<-c) // Retorna
+}
