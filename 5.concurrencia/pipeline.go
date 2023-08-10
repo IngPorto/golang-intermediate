baa368635cca7c728210889cda72597e3daad4e5
ingporto <ing.portocarrero@gmail.com>
2023-08-10 17:19:58 -0500

    canales de solo lentura y solo escritura

---
+++ 5.concurrencia/pipeline.go
@@ -0,0 +1,32 @@
+// Definiendo channels de lectura y escritura
+
+package main
+
+func Generator (c chan<- int) {
+	for i := 1; i < 10; i++ {
+		c <- i
+	}
+	close(c)
+}
+
+func Double (in <-chan int, out chan int ) {
+	for value := range in {
+		out <- 2 * value
+	}
+	close(out)
+}
+
+func Print (c chan int){
+	for value := range c {
+		println(value)
+	}
+}
+
+func main (){
+	generator := make(chan int)
+	double := make(chan int)
+
+	go Generator(generator)
+	go Double(generator, double)
+	Print(double) // el programa se bloquea en esta linea
+}
